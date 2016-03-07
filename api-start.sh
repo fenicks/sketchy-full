d68bcc0005bb9a08d31277061fc9b193c9806c23
Christian Kakesa <christian.kakesa@gmail.com>
2016-03-08 00:45:36 +0100

    First commit.

---
+++ api-start.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+current_dir=$(cd $(dirname "$0")/.. && pwd)
+
+sudo service postgresql start
+sudo -u postgres psql -c "CREATE USER sketchy WITH PASSWORD 'sketchy';" > /dev/null 2>&1
+sudo -u postgres psql -c "CREATE DATABASE sketchy OWNER sketchy;" > /dev/null 2>&1
+
+redis-server &
+
+cd ${current_dir}
+source sketchenv/bin/activate
+python manage.py create_db
+
+celery -A sketchy.celery worker &
+
+python manage.py runserver --host 0.0.0.0 --port 8000
